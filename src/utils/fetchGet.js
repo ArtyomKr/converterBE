946e4fd57a81ce66bcfe50dcea0b31bc535808e4
ArtyomKr <ArtyomKr@users.noreply.github.com>
2022-06-13 20:29:50 +0300

    feat: add custom fetch

---
+++ src/utils/fetchGet.js
@@ -0,0 +1,25 @@
+import https from 'https';
+
+function fetchGet(options) {
+  options.method = 'GET';
+  options.port = 443;
+
+  return new Promise((resolve, reject) => {
+    let response = ''
+
+    const req = https.get(options, (res) => {
+      res.on('data', chunk => {
+        response += chunk;
+      });
+      res.on('end', () => resolve({
+        body: JSON.parse(response),
+        code: res.statusCode
+      }));
+    })
+
+    req.on('error', err => reject(err));
+    req.end();
+  })
+}
+
+export default fetchGet;
