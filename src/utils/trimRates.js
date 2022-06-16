5082c8f106828d4a14fb14dc0fe49e0b9ec4f031
ArtyomKr <ArtyomKr@users.noreply.github.com>
2022-06-16 18:07:02 +0300

    feat: add null value detection to trimming function

--- src/utils/trimRates.js
+++ src/utils/trimRates.js
@@ -1,8 +1,9 @@
 function trimRates(ratesData) {
   const trimmedArr = Object.entries(ratesData).map(([key, value]) => {
+    if (!value) return [key, 0];
     const string = value.toFixed(4).toString();
     const trimmed = string.replace(/0+$/,'');
-    return [key, Number(trimmed)]
+    return [key, Number(trimmed)];
   })
 
   return Object.fromEntries(trimmedArr);
